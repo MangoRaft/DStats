66a09b423cf1f2265dded40f85fa7e60ad93cbcf
FLYBYME <mangoraft@gmail.com>
2018-05-27 09:13:37 -0400

    Just aggregate stats dont send them

--- example.js
+++ example.js
@@ -1,36 +1,34 @@
 var Docker = require('dockerode');
 var docker = new Docker({
-	socketPath : '/var/run/docker.sock'
+    socketPath: '/var/run/docker.sock'
 });
 
 var DStats = require('./lib/dstats');
 
 var container = docker.getContainer('0ff3a3737ba41468da5c1864d2107b0a3effbb59d8708f30fe313fd1cc1bdd3b');
 
-var dstats = new DStats({
-	host : '127.0.0.1',
-	port : 8125,
-	key : '2dcd78e1-cea9-4bab-8e5c-6ecbc2579479'
-});
-
+var dstats = new DStats({});
+dstats.on('aggregated', function(stats){
+    console.log(stats)
+})
 container.stats({
-	stream : true
-}, function(error, stream) {
-	if (error) {
+    stream: true
+}, function (error, stream) {
+    if (error) {
 
-		throw error
-	}
+        throw error
+    }
 
-	function onData(data) {
-		try {
-			var json = JSON.parse(data.toString());
-		} catch(err) {
-			return console.log(err);
-		}
+    function onData(data) {
+        try {
+            var json = JSON.parse(data.toString());
+        } catch (err) {
+            return console.log(err);
+        }
 
-		dstats.stats(json);
-	}
+        dstats.stats(json);
+    }
 
 
-	stream.on('data', onData);
+    stream.on('data', onData);
 });
